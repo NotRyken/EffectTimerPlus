21253c0eba5f7be3955a38e22403229b580ad62c
NotRyken <notryken@gmail.com>
2024-01-09 10:00:23 +0800

    Initial NeoForge support

--- neoforge/src/main/java/notryken/effectinfo/EffectInfoNeoForge.java
+++ neoforge/src/main/java/notryken/effectinfo/EffectInfoNeoForge.java
@@ -1,18 +1,15 @@
 package notryken.effectinfo;
 
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.ConfigScreenHandler;
-import notryken.effectinfo.gui.screen.GlobalConfigScreen;
 
-@Mod(Constants.MOD_ID)
+@Mod(EffectInfo.MOD_ID)
 public class EffectInfoNeoForge {
     public EffectInfoNeoForge() {
-        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
-                () -> new ConfigScreenHandler.ConfigScreenFactory(
-                        (client, parent) -> new GlobalConfigScreen(parent))
-                );
-
-        EffectInfo.init();
+//        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
+//                () -> new ConfigScreenHandler.ConfigScreenFactory(
+//                        (client, parent) -> new GlobalConfigScreen(parent))
+//                );
+//
+//        EffectInfo.init();
     }
 }
