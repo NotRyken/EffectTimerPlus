1bad32c713bab13b7597ae82fb9e2701192ad121
NotRyken <notryken@gmail.com>
2024-01-20 13:46:49 +0800

    Update to mc1.20.3-1.20.4

--- common/src/main/java/notryken/effecttimerplus/gui/component/PotencyListWidget.java
+++ common/src/main/java/notryken/effecttimerplus/gui/component/PotencyListWidget.java
@@ -23,9 +23,9 @@ public class PotencyListWidget extends AbstractListWidget  {
     protected AbstractListWidget.Entry resetButton;
 
 
-    public PotencyListWidget(Minecraft minecraft, int width, int height, int top, int bottom,
+    public PotencyListWidget(Minecraft minecraft, int width, int height, int y,
                              int itemHeight, ConfigScreen parent) {
-        super(minecraft, width, height, top, bottom, itemHeight, parent);
+        super(minecraft, width, height, y, itemHeight, parent);
 
         int unitWidth = 200;
         int unitHeight = 18;
